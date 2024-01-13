3d0a97d7f7c1777e959780ec696fa55b67ebeab2
Milan Malfait <m.malfait@ucl.ac.uk>
2024-01-13 14:12:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Template for a minimal cmake project
+
+Some boilerplate `cmake` files so I don't have to remember how to write them.
---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "add.hpp"
+
+int main() {
+    int x = 40;
+    int y = 2;
+
+    std::cout << "x + y = " << add(x, y) << std::endl;
+    return 0;
+}
